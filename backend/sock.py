5b327d225cbdf83c4bc18f386d21190c69a5c973
FTU_ACMeow <acmeow@qq.com>
2017-12-25 12:38:44 +0800

    sock:make some bugs

--- backend/sock.py
+++ backend/sock.py
@@ -4,6 +4,7 @@ import queue
 import threading
 from .constants import *
 import time
+import os
 
 def extend_one_second() :
     time.sleep(default_extend)
@@ -47,6 +48,10 @@ class Sock:
         self.check_active(ip)
 
     def get(self, ip):
+        while self.q_empty(ip):
+            if DEBUG:
+                print('Waiting for message from IP =', ip)
+            extend_one_second()
         r = self.queue[ip].get()
         self.check_active(ip)
         return r
@@ -58,6 +63,8 @@ class Sock:
         self.check_active(ip)
 
     def hb(self, ip):
+        if DEBUG:
+            print('Get heartbeat from IP =', ip)
         if ip not in self.status.keys():
             self.status[ip] = time.time()
         self.status[ip] = time.time()
@@ -87,11 +94,13 @@ class Sock:
                 skt.sendall(message)
                 break
             except:
-                pass
+                if DEBUG:
+                    print('Send ERROR!')
             extend_one_second()
 
     def connect (self, ip):
         self.send(ip, heartbeat)
+        self.hb(ip)
 
     def heart_beat(self):
         while True:
@@ -139,7 +148,7 @@ class Sock:
                     break
             except:
                 if DEBUG:
-                    print('Send error. Resend.')
+                    print('Send file error. Resend.')
             extend_one_second()
         self.unlock_active(ip)
         return hashlib.md5(data).hexdigest()
@@ -185,9 +194,16 @@ class Sock:
 
                 md5 = hashlib.md5(data).hexdigest()
 
-                save_file = open(work_dir+'\\'+md5, 'wb')
-                save_file.write(data)
-                save_file.close()
+                try:
+                    if not os.path.exists(work_dir):
+                        os.mkdir(work_dir)
+                    save_file = open(work_dir+'\\'+md5, 'wb')
+                    save_file.write(data)
+                    save_file.close()
+                except:
+                    if DEBUG:
+                        print('Cannot save in file! filename =', work_dir+'\\'+md5)
+                    raise IOError
 
                 self.send(ip, accepted)
                 break
